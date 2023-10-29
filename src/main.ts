b1ba3e4eba5f49b243d424d37bc649c7ec802d72
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2023-10-29 09:54:14 +0100

    Add morgan http request logging

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { AppModule } from './app.module';
 import { createServer } from 'http';
-
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable HTTP request logging using Morgan
+  // Options include: 'combined', 'common', 'dev', 'tiny', 'combined' logs more details
+  app.use(morgan('dev'));
 
   // Enable CORS for the entire application
   // app.enableCors({
